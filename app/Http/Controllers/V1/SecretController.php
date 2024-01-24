2ee8f37cca282416816caa5351fa6f60f8e7bf2b
Your Name <you@example.com>
2024-01-24 05:16:32 +0100

    Init

---
+++ app/Http/Controllers/V1/SecretController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SecretController extends Controller
+{
+
+    public function add(Request $request)
+    {
+
+    }
+
+    public function view()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+}
