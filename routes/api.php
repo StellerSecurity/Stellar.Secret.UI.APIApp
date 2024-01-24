cca20597c65f19282ae3f88e503faa2ad2f072e8
Your Name <you@example.com>
2024-01-25 00:13:58 +0100

    defalt

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('secretcontroller')->group(function () {
+        Route::controller(\App\Http\Controllers\V1\SecretController::class)->group(function () {
+            Route::post('add', 'add');
+            Route::get('secret', 'find');
+            Route::delete('delete', 'delete');
+        });
+    });
+
 });
