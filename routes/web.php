dd35a0e9897690fdb3d8d7234a3f1d55765a7f07
Bob <150491881+stellar-security-os@users.noreply.github.com>
2025-02-06 00:30:21 +0100

    Cache clear

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('route:cache');
+    Artisan::call('cache:clear');
     return "";
 });
+
 
